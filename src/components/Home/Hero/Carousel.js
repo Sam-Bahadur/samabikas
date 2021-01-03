7c90187d1c8fe7aa9518dc31de4768093e6425b3
unknown <pradipktimsina@gmail.com>
2021-01-03 18:19:31 +0545

    jan 3-v2

---
+++ src/components/Home/Hero/Carousel.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import img from "../../../assets/images/hero1.jpg"
+ 
+export default function SimpleSlider() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  return (
+      <div className="my-8">
+    <Slider {...settings}>
+      <div>
+        <img src={img} />
+      </div>
+      <div>
+      <img src={img} />
+      </div>
+    </Slider>
+      </div>
+  );
+}
--- src/components/Home/Hero/Hero.js
+++ src/components/Home/Hero/Hero.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import {AiFillPhone} from "react-icons/ai"
 import {FiMail} from "react-icons/fi"
+import {AiFillFacebook,AiFillTwitterSquare,AiFillLinkedin,AiFillYoutube} from "react-icons/ai"
+import Carousel from "./Carousel"
 
 export default function Hero() {
     return (
+        <>
         <div className="flex flex-wrap justify-between">
             <div className="">
                 {/* <img src="https://picsum.photos/200" /> */}
                 <span className="text-blue-700 text-5xl font-bold">Samabikas</span><span className="font-bold text-green-700 text-5xl">Nepal</span>
                 <h3 className="text-gray-600 opacity-80">For Equity, Equality and Justice</h3>
             </div>
-            <div className="">
-                 <span className="text-gray-600"><AiFillPhone className="inline" />9843534725 <FiMail className="inline ml-2" /> samabikas@gmail.com</span>
+            <div className="self-center">
+                 <span className="text-gray-600"><AiFillPhone className="inline" />9843534725 <FiMail className="inline ml-2" /> samabikas@gmail.com <AiFillFacebook className="inline ml-2" /><AiFillTwitterSquare className="inline ml-2" /><AiFillLinkedin className="inline ml-2" /><AiFillYoutube className="inline ml-2" /> </span>
             </div>
-            
         </div>
+        <Carousel />
+</>
     )
 }
